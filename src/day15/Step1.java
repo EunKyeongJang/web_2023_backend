b449f5497b7be786db896710d539175210f2232f
EunKyeongJang <jjjang1030@naver.com>
2024-01-22 10:52:21 +0900

    240122_1052

---
+++ src/day15/Step1.java
@@ -0,0 +1,7 @@
+package day15;
+
+public class Step1 {//c start
+    public static void main(String[] args) {//m start
+
+    }//m end
+}//c end
---
+++ src/day15/Texi.java
@@ -0,0 +1,8 @@
+package day15;
+
+public class Texi extends Vehicle{
+    @Override
+    public void run() {
+        System.out.println("택시가 달립니다.");
+    }
+}
